16f8b69ed4a2872baf66629fcf0b388160f61e05
sanirurajapaksha <sanirurajapaksha456@gmail.com>
2026-03-27 00:12:39 +0530

    test: add initial test stubs for core modules

---
+++ services/telemedicine-notification-service/src/test/java/com/ezclinic/telemedicine/TelemedicineNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezclinic.telemedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicineNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
